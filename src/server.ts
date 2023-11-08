868996931d9d5295e68452bad6ed0e7dc90fdee9
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-08 09:11:44 +0100

    Add user and message endpoints to server

--- src/server.ts
+++ src/server.ts
@@ -6,20 +6,55 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.port || 3000;
 
-app.get('/', (req, res) => {
-	return res.send('Received a GET HTTP method');
+interface User {
+	id: string;
+	username: string;
+}
+
+interface Message {
+	id: string;
+	text: string;
+	userId: string;
+}
+
+const users: Record<string, User> = {
+	'1': {
+		id: '1',
+		username: 'Robin Wieruch',
+	},
+	'2': {
+		id: '2',
+		username: 'Dave Davids',
+	},
+};
+
+const messages: Record<string, Message> = {
+	'1': {
+		id: '1',
+		text: 'Hello World',
+		userId: '1',
+	},
+	'2': {
+		id: '2',
+		text: 'By World',
+		userId: '2',
+	},
+};
+
+app.get('/users', (req, res) => {
+	return res.send(Object.values(users));
 });
 
-app.post('/', (req, res) => {
-	return res.send('Received a POST HTTP method');
+app.get('/users/:userId', (req, res) => {
+	return res.send(users[req.params.userId]);
 });
 
-app.put('/', (req, res) => {
-	return res.send('Received a PUT HTTP method');
+app.get('/messages', (req, res) => {
+	return res.send(Object.values(messages));
 });
 
-app.delete('/', (req, res) => {
-	return res.send('Received a DELETE HTTP method');
+app.get('/messages/:messageId', (req, res) => {
+	return res.send(messages[req.params.messageId]);
 });
 
 app.listen(port, () => {
